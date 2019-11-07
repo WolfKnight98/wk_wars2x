d1159e8c2c6fc87dcc3edef37a2a71bf90bc3e79
Dan <WolfKnight98@users.noreply.github.com>
2019-11-07 20:31:17 +0000

    Updated some of the sort modes to be a bit more accurate

--- cl_radar.lua
+++ cl_radar.lua
@@ -81,10 +81,30 @@ RADAR.rayTraces = {
 }
 
 RADAR.sorting = {
-	[1] = { name = "CLOSEST", func = function( a, b ) return a.dist < b.dist end }, 
-	[2] = { name = "FASTEST", func = function( a, b ) return a.speed > b.speed end }, 
-	[3] = { name = "LARGEST", func = function( a, b ) return a.size > b.size end },
-	[4] = { name = "LARGEST & FASTEST", func = function( a, b ) return a.size > b.size and a.speed > b.speed end }
+	[1] = { 
+		name = "CLOSEST", 
+		func = function( a, b ) return a.dist < b.dist end 
+	}, 
+	[2] = { 
+		name = "FASTEST", 
+		func = function( a, b ) return a.speed > b.speed end 
+	}, 
+	[3] = { 
+		name = "LARGEST", 
+		func = function( a, b ) return a.size > b.size + 1.0 end
+	},
+	[4] = { 
+		name = "LARGEST & FASTEST", 
+		func = function( a, b ) 
+			if ( a.size > b.size + 2.0 ) then 
+				return true 
+			elseif ( a.size - b.size <= 2.0 ) then 
+				return false 
+			end 
+
+			return a.speed > b.speed 
+		end 
+	}
 }
 
 --[[------------------------------------------------------------------------
