861f5663e13be6a88574e3dbaf50e282bdb83805
Dan <WolfKnight98@users.noreply.github.com>
2019-12-08 00:33:04 +0000

    Update sv_version_check.lua

--- sv_version_check.lua
+++ sv_version_check.lua
@@ -1,7 +1,7 @@
 --[[-----------------------------------------------------------------------
 
-    Wraith ARS 2X
-    Created by WolfKnight
+	Wraith ARS 2X
+	Created by WolfKnight
 
 -----------------------------------------------------------------------]]--
 
@@ -10,16 +10,16 @@ function GetCurrentVersion()
 end 
 
 PerformHttpRequest( "https://wolfknight98.github.io/wk_wars2x/version.txt", function( err, text, headers )
-    Citizen.Wait( 2000 )
+	Citizen.Wait( 2000 )
 
-    local curVer = GetCurrentVersion()
-    
-    print( "\n//\n|| Current Wraith ARS 2X Version: " .. curVer )
+	local curVer = GetCurrentVersion()
+	
+	print( "\n//\n|| Current Wraith ARS 2X Version: " .. curVer )
 	print( "|| Latest Wraith ARS 2X Version: " .. text .."\n||" )
 	
 	if ( text ~= curVer ) then
-        print( "|| ^1Your Wraith ARS 2X version is outdated, visit the FiveM forum post to get the latest version.\n^0\\\\" )
+		print( "|| ^1Your Wraith ARS 2X version is outdated, visit the FiveM forum post to get the latest version.\n^0\\\\" )
 	else
 		print( "|| ^2Wraith ARS 2X is up to date!\n^0\\\\" )
-    end
+	end
 end )
