753927800505766a6fe0dee938dcfad4b4e71881
Dan <WolfKnight98@users.noreply.github.com>
2019-12-10 21:25:45 +0000

    Update sv_version_check.lua

--- sv_version_check.lua
+++ sv_version_check.lua
@@ -6,13 +6,14 @@
 -----------------------------------------------------------------------]]--
 
 local label = 
-[[//
-||    __          __        _ _   _                _____   _____   ___ __   __
-||    \ \        / /       (_) | | |         /\   |  __ \ / ____| |__ \\ \ / /
-||     \ \  /\  / / __ __ _ _| |_| |__      /  \  | |__) | (___      ) |\ V / 
-||      \ \/  \/ / '__/ _` | | __| '_ \    / /\ \ |  _  / \___ \    / /  > <  
-||       \  /\  /| | | (_| | | |_| | | |  / ____ \| | \ \ ____) |  / /_ / . \ 
-||        \/  \/ |_|  \__,_|_|\__|_| |_| /_/    \_\_|  \_\_____/  |____/_/ \_\]]
+[[ 
+  //
+  ||    __          __        _ _   _                _____   _____   ___ __   __
+  ||    \ \        / /       (_) | | |         /\   |  __ \ / ____| |__ \\ \ / /
+  ||     \ \  /\  / / __ __ _ _| |_| |__      /  \  | |__) | (___      ) |\ V / 
+  ||      \ \/  \/ / '__/ _` | | __| '_ \    / /\ \ |  _  / \___ \    / /  > <  
+  ||       \  /\  /| | | (_| | | |_| | | |  / ____ \| | \ \ ____) |  / /_ / . \ 
+  ||        \/  \/ |_|  \__,_|_|\__|_| |_| /_/    \_\_|  \_\_____/  |____/_/ \_\]]
 
 function GetCurrentVersion()
 	return GetResourceMetadata( GetCurrentResourceName(), "version" )
@@ -25,12 +26,12 @@ PerformHttpRequest( "https://wolfknight98.github.io/wk_wars2x/version.txt", func
 
 	local curVer = GetCurrentVersion()
 	
-	print( "||\n||    Current version: " .. curVer )
-	print( "||    Latest version: " .. text .."\n||" )
+	print( "  ||\n  ||    Current version: " .. curVer )
+	print( "  ||    Latest version: " .. text .."\n  ||" )
 	
 	if ( text ~= curVer ) then
-		print( "||    ^1Your Wraith ARS 2X version is outdated, visit the FiveM forum post to get the latest version.\n^0\\\\" )
+		print( "  ||    ^1Your Wraith ARS 2X version is outdated, visit the FiveM forum post to get the latest version.\n^0  \\\\\n" )
 	else
-		print( "||    ^2Wraith ARS 2X is up to date!\n^0||\n\\\\" )
+		print( "  ||    ^2Wraith ARS 2X is up to date!\n^0  ||\n  \\\\\n" )
 	end
 end )
