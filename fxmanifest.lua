c8de750a1e1381a5b674718e4de8a6191a5bdec7
Dan <WolfKnight98@users.noreply.github.com>
2020-02-06 11:52:05 +0000

    Fixed fxmanifest.lua not working on linux servers

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,31 +5,38 @@
 
 -----------------------------------------------------------------------]]--
 
-fx_version 'adamant'
-game 'gta5'
+-- Define the FX Server version and game type
+fx_version "adamant"
+game "gta5"
 
-name 'Wraith ARS 2X'
-description 'Police radar and plate reader system for FiveM'
-author 'WolfKnight'
-version 'beta4'
+-- Define the resource metadata
+name "Wraith ARS 2X"
+description "Police radar and plate reader system for FiveM"
+author "WolfKnight"
+version "beta4"
 
+-- Include the files
 files {
     "nui/radar.html", 
     "nui/radar.css", 
     "nui/jquery-3.4.1.min.js", 
     "nui/radar.js",
-    "nui/images/*",
-    "nui/images/plates/*",
-    "nui/fonts/*",
-    "nui/sounds/*"
+    "nui/images/*.png",
+    "nui/images/plates/*.png",
+    "nui/fonts/*.ttf",
+    "nui/fonts/Segment7Standard.otf",
+    "nui/sounds/*.ogg"
 }
 
+-- Set the NUI page
 ui_page "nui/radar.html"
 
-server_script 'sv_version_check.lua'
-server_script 'sv_saving.lua'
+-- Run the server scripts
+server_script "sv_version_check.lua"
+server_script "sv_saving.lua"
 
-client_script 'config.lua'
-client_script 'cl_utils.lua'
-client_script 'cl_radar.lua'
-client_script 'cl_plate_reader.lua'
+-- Run the client scripts
+client_script "config.lua"
+client_script "cl_utils.lua"
+client_script "cl_radar.lua"
+client_script "cl_plate_reader.lua"
