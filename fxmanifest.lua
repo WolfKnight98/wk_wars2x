dd30bce1d19bf309f3e30c22ea285949ac2d5b64
Dan <WolfKnight98@users.noreply.github.com>
2020-03-13 21:17:25 +0000

    Update version number
    If you're confused why the version number jumped so high, it's because I'm using semantic versioning.

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -38,7 +38,7 @@ game "gta5"
 name "Wraith ARS 2X"
 description "Police radar and plate reader system for FiveM"
 author "WolfKnight"
-version "1.0.0"
+version "1.9.4"
 
 -- Include the files
 files {
