1cf7386d28d0a32fdc94f49d5853e6ae3892f129
Dan <WolfKnight98@users.noreply.github.com>
2020-03-14 13:59:47 +0000

    Changes so far considered 1 major revision

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -38,7 +38,7 @@ game "gta5"
 name "Wraith ARS 2X"
 description "Police radar and plate reader system for FiveM"
 author "WolfKnight"
-version "1.5.4"
+version "1.1.0"
 
 -- Include the files
 files {
