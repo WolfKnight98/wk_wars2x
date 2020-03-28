74539874c3e877d9b4b97f6b05b696181b672b49
Dan <WolfKnight98@users.noreply.github.com>
2020-03-28 21:56:34 +0000

    Updated version number
    x.2.x = new keymaps, adjusted display update speed
    x.x.3 = fixed heading check

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -38,7 +38,7 @@ game "gta5"
 name "Wraith ARS 2X"
 description "Police radar and plate reader system for FiveM"
 author "WolfKnight"
-version "1.1.2"
+version "1.2.3"
 
 -- Include the files
 files {
