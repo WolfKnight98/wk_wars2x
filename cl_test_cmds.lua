f6034a4bed881a352c68a48f34f44670d681e05f
Dan <WolfKnight98@users.noreply.github.com>
2020-11-30 09:58:18 +0000

    Added /rtpv debug command

--- cl_test_cmds.lua
+++ cl_test_cmds.lua
@@ -3,3 +3,9 @@ RegisterCommand( "rtpc", function( source, args, rawCommand )
 	CONFIG.allow_passenger_control = not CONFIG.allow_passenger_control
 end, false )
 TriggerEvent( "chat:addSuggestion", "/rtpc", "Toggle CONFIG.allow_passenger_control" )
+
+-- Radar Toggle Passenger View
+RegisterCommand( "rtpv", function( source, args, rawCommand )
+	CONFIG.allow_passenger_view = not CONFIG.allow_passenger_view
+end, false )
+TriggerEvent( "chat:addSuggestion", "/rtpv", "Toggle CONFIG.allow_passenger_view" )
