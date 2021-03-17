9b6279fd5f2ecb97ec64e198f82e80087a661810
Dan <WolfKnight98@users.noreply.github.com>
2021-03-17 20:31:30 +0000

    docs: update version number to 1.3.0 ready for release

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -38,7 +38,7 @@ game "gta5"
 name "Wraith ARS 2X"
 description "Police radar and plate reader system for FiveM"
 author "WolfKnight"
-version "beta"
+version "1.3.0"
 
 -- Include the files
 files {
